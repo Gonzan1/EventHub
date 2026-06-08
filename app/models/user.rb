bc90442f27fd268ce8108b6b2560945f81e4dde1
Elchapax24 <taarratia@miuandes.cl>
2026-06-08 13:19:05 -0400

    Install devise and cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Asociaciones
   has_many :events, foreign_key: 'organizer_id', dependent: :destroy
   has_many :registrations, dependent: :destroy
